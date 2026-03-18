589c59827663353442687c5f997be8a1a6655772
CammyParakeet <cammy.sheesh@gmail.com>
2026-03-19 12:57:09 +1300

    initial new project setup

---
+++ build.gradle.kts
@@ -0,0 +1,95 @@
+import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
+import net.minecrell.pluginyml.paper.PaperPluginDescription
+
+plugins {
+    id("java")
+    id("java-library")
+
+    alias(libs.plugins.shadow)
+    alias(libs.plugins.indra)
+    alias(libs.plugins.plugin.yml)
+}
+
+group = "creatorsplash.oxygenheist"
+version = "1.0.0"
+
+repositories {
+    mavenCentral()
+    maven("https://oss.sonatype.org/content/repositories/snapshots/")
+    maven("https://repo.papermc.io/repository/maven-public/")
+    maven("https://repo.codemc.io/repository/maven-releases/")
+    maven("https://repo.triumphteam.dev/snapshots")
+    maven("https://jitpack.io")
+    maven("https://repo.extendedclip.com/releases/")
+}
+
+dependencies {
+    compileOnly(libs.paper.api)
+
+    // GUI
+    paperLibrary(libs.triumph.gui)
+
+    // Commands
+    paperLibrary(libs.cloud.paper)
+    paperLibrary(libs.cloud.annotations)
+
+    // Development
+    compileOnly(libs.lombok)
+    annotationProcessor(libs.lombok)
+
+    annotationProcessor(libs.auto.service)
+    compileOnly(libs.auto.service.annotations)
+
+    compileOnly(libs.jetbrains.annotations)
+}
+
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(21))
+    }
+}
+
+tasks {
+    build {
+        dependsOn(shadowJar)
+        finalizedBy("exportJars")
+    }
+
+    withType<JavaCompile> {
+        options.release.set(21)
+        options.encoding = Charsets.UTF_8.name()
+        options.compilerArgs = listOf("-parameters")
+    }
+
+    register<Copy>("exportJars") {
+        val shadowJar = named<ShadowJar>("shadowJar")
+
+        dependsOn(shadowJar)
+
+        from(
+            shadowJar.flatMap { it.archiveFile },
+        )
+
+        into(rootProject.layout.projectDirectory.dir("target"))
+    }
+}
+
+paper {
+    name = "OxygenHeist"
+
+    apiVersion = "1.21"
+    //version = "Git-${indraGit.commit()?.name?.take(7) ?: "unknown"}"
+    version = "1.0.0"
+
+    main = "creatorsplash.oxygenheist.platform.paper.OxygenHeistPlugin"
+
+    loader = "creatorsplash.oxygenheist.platform.paper.bootstrap.LibLoader"
+    generateLibrariesJson = true
+
+    serverDependencies {
+        register("PlaceholderAPI") {
+            required = false
+            load = PaperPluginDescription.RelativeLoadOrder.AFTER
+        }
+    }
+}
---
+++ src/main/java/creatorsplash/oxygenheist/platform/paper/OxygenHeistPlugin.java
@@ -0,0 +1,17 @@
+package creatorsplash.oxygenheist.platform.paper;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class OxygenHeistPlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // TODO
+    }
+
+    @Override
+    public void onDisable() {
+        // TODO
+    }
+
+}
